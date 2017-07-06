fa39363dc75262d4b7642dcb9f729411d93f8be6
JosueCuberoSanchez <josue.cubero@ucr.ac.cr>
2017-07-06 14:39:08 -0600

    Initial Commit

---
+++ src/main/java/cr/ac/ucr/ecci/ci1310/cache/Main.java
@@ -0,0 +1,10 @@
+package cr.ac.ucr.ecci.ci1310.cache;
+
+/**
+ * Created by Josue Cubero on 06/07/2017.
+ */
+public class Main {
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+    }
+}
