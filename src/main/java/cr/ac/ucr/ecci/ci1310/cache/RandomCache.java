28a45867f67724f314fe4c187491332ad0c69e3a
JosueCuberoSanchez <josue.cubero@ucr.ac.cr>
2017-07-07 15:06:24 -0600

    Constructures

--- src/main/java/cr/ac/ucr/ecci/ci1310/cache/RandomCache.java
+++ src/main/java/cr/ac/ucr/ecci/ci1310/cache/RandomCache.java
@@ -1,9 +1,30 @@
 package cr.ac.ucr.ecci.ci1310.cache;
 
+import java.util.Map;
+
 /**
  * Created by bjgd9 on 7/7/2017.
  */
 public class RandomCache <K , V> implements  Cache <K , V>{
+
+    K key;
+    V value;
+    private int size;
+    private String name;
+    private int maxItems;
+    private Double objectLifespan;
+    private Double cacheLifespan;
+    private Map<K,V> map;
+    //private RandomCache<Integer, String> rc = new RandomCache<Integer, String>()
+
+
+    public RandomCache(int size,String name,int maxItems,Double objectLifespan,Double cacheLifespan){
+        this.size = size;
+        this.name = name;
+        this.maxItems = maxItems;
+        this.objectLifespan = objectLifespan;
+        this.cacheLifespan = cacheLifespan;
+    }
 
     public String getName() {
         return null;
