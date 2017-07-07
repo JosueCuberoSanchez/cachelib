4bb57e81f5789298166e188274ef4b886413f16e
bgd95 <bjgd95@gmail.com>
2017-07-07 14:26:57 -0600

    Cascaron 1

---
+++ src/main/java/cr/ac/ucr/ecci/ci1310/cache/LIFOCache.java
@@ -0,0 +1,29 @@
+package cr.ac.ucr.ecci.ci1310.cache;
+
+import javafx.util.Pair;
+
+/**
+ * Created by bjgd9 on 7/7/2017.
+ */
+public class LIFOCache implements  Cache<String ,Pair<Integer,String>> {
+
+    public String getName() {
+        return null;
+    }
+
+    public Pair<Integer, String> get(String var1) {
+        return null;
+    }
+
+    public void put(String var1, Pair<Integer, String> var2) {
+
+    }
+
+    public void evidict() {
+
+    }
+
+    public void clear() {
+
+    }
+}
---
+++ src/main/java/cr/ac/ucr/ecci/ci1310/cache/RandomCache.java
@@ -0,0 +1,26 @@
+package cr.ac.ucr.ecci.ci1310.cache;
+
+/**
+ * Created by bjgd9 on 7/7/2017.
+ */
+public class RandomCache implements  Cache <String,String> {
+    public String getName() {
+        return null;
+    }
+
+    public String get(String var1) {
+        return null;
+    }
+
+    public void put(String var1, String var2) {
+
+    }
+
+    public void evidict() {
+
+    }
+
+    public void clear() {
+
+    }
+}
